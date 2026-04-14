55096bef1ceda18391df63d6698f7c3be69cf74d
Elisa-Reyes <elisa_3141240138@utd.edu.mx>
2026-04-13 23:58:36 -0600

    pls commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// const express = require("express");
+// const cors = require("cors");
+// const session = require("express-session");
+// const bcrypt = require("bcrypt");
+
+// const app = express();
+
+// app.use(express.json());
+// app.use(cors());
