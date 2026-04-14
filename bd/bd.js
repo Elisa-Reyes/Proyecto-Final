4c8d43aff733d897c6cc390de0a842614459f451
Elisa-Reyes <elisa_3141240138@utd.edu.mx>
2026-04-14 01:20:35 -0600

    yarn add moment + bd

---
+++ bd/bd.js
@@ -0,0 +1,11 @@
+//main table content :D
+//CREATE DATABASE todo;
+// USE todo;
+// CREATE TABLE usuarios (id INT AUTO_INCREMENT PRIMARY KEY,
+//  nombre VARCHAR(100), email VARCHAR(100), password VARCHAR(100));
+//  CREATE TABLE tasks (id INT AUTO_INCREMENT PRIMARY KEY,
+//                      completed BOOLEAN DEFAULT FALSE,
+//                      usuario_id INT,
+//                      title VARCHAR(100) NOT NULL,
+//                      description TEXT,
+//                     FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE);
--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const session = require("express-session");
 // const bcrypt = require("bcrypt");
+//also, we added moment: yarn add moment
 
 const app = express();
 
