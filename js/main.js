52f9f24d102ae2e27dc26b919daa248bbc492c4c
Elisa-Reyes <elisa_3141240138@utd.edu.mx>
2026-04-20 13:10:07 -0600

    registro hecho

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
+const bcrypt = require("bcrypt");
+const session = require("express-session");
+const express = require("express");
+const router = express.Router();
+const db = require("../bd/bd");
+const moment = require("moment");
+
 //also, we added moment: yarn add moment
 //all tasks here
