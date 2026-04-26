44d937bbdc1790fcde3701f81b69202d6c99b399
DianaCesenas <diana.crivera9@gmail.com>
2026-04-26 02:16:32 -0600

    implementando el main

--- bd/bd.js
+++ bd/bd.js
@@ -1,5 +1,5 @@
-//main table content :D
-//CREATE DATABASE todo;
+// main table content :D
+// CREATE DATABASE todo;
 // USE todo;
 // CREATE TABLE usuarios (id INT AUTO_INCREMENT PRIMARY KEY,
 //  nombre VARCHAR(100), email VARCHAR(100), password VARCHAR(100));
